52fcc93ca2bcae657ad554557972b3a0cae4bdc6
Kapunahele Wong <kapunahelewong@gmail.com>
2021-07-26 20:04:39 -0400

    add code for copy button (#97)
    Signed-off-by: Kapunahele Wong <kapunahelewong@gmail.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,3 +27,59 @@ function toggleNav() {
     leftnavToggle.classList.remove("open");
   }
 }
+
+// Add copy button on code blocks
+// This code is kindly shared under the MIT License by Tom Spencer in their most excellent [tutorial](https://www.tomspencer.dev/blog/2018/09/14/adding-click-to-copy-buttons-to-a-hugo-powered-blog/).
+
+(function() {
+  'use strict';
+
+  if(!document.queryCommandSupported('copy')) {
+    return;
+  }
+
+  function confirmationMsg(el, msg) {
+    el.textContent = msg;
+    setTimeout(function() {
+      el.textContent = "Copy";
+    }, 1200);
+  }
+
+  function selectText(node) {
+    var selection = window.getSelection();
+    var range = document.createRange();
+    range.selectNodeContents(node);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    return selection;
+  }
+  
+  // Create a copy button 
+  function addCopyButton(containerEl) {
+    var copyBtn = document.createElement("button");
+    copyBtn.className = "highlight-copy-btn";
+    copyBtn.textContent = "Copy";
+
+    var codeEl = containerEl.firstElementChild;
+    copyBtn.addEventListener('click', function() {
+      try {
+        var selection = selectText(codeEl);
+        document.execCommand('copy');
+        selection.removeAllRanges();
+
+        confirmationMsg(copyBtn, 'Copied!')
+      } catch(e) {
+        console && console.log(e);
+        confirmationMsg(copyBtn, 'Failed.')
+      }
+    });
+
+    containerEl.appendChild(copyBtn);
+  }
+
+  // Add copy button to code blocks
+  var highlightBlocks = document.getElementsByClassName('highlight');
+  Array.prototype.forEach.call(highlightBlocks, addCopyButton);
+})();
+
+
